ef16c357b72d7eb6a476c2b0191ad4b80d80e74f
Collin Smith <cmsmith.tx@gmail.com>
2019-12-16 11:18:46 -0600

    connecting to mysql db

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php 
+  // connect to database
+
+  $conn = mysqli_connect('localhost', 'collin', 'test1234', 'phpizza');
+
+  if(!$conn){
+    echo 'Database not connected' . mysqli_connect_error();
+  }
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <?php include 'templates/header.php'; ?>
