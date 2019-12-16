2b9551a92aed7166697a7964a413cd88130a49d2
Collin Smith <cmsmith.tx@gmail.com>
2019-12-16 15:04:59 -0600

    can now delete items from db'

--- details.php
+++ details.php
@@ -2,22 +2,38 @@
 
   include('config/db_connect.php');
   //check GET request id param
+  
+  if(isset($_POST['delete'])){
+
+    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+    $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+    if(mysqli_query($conn, $sql)){
+      //success
+      header('Location: index.php');
+    } else {
+      //failure
+      echo 'query error: ' . mysqli_error($conn);
+    }
+  }
+  
   if(isset($_GET['id'])){
     $id = mysqli_real_escape_string($conn, $_GET['id']);
+    
+    //make sql
+    $sql = "SELECT * FROM pizzas WHERE id = $id";
+    
+    //get query results
+    $result = mysqli_query($conn, $sql);
+    
+    //fetch result in array format
+    $pizza = mysqli_fetch_assoc($result);
+    
+    mysqli_free_result($result);
+    mysqli_close($conn);
+    
   }
-
-  //make sql
-  $sql = "SELECT * FROM pizzas WHERE id = $id";
-
-  //get query results
-  $result = mysqli_query($conn, $sql);
-
-  //fetch result in array format
-  $pizza = mysqli_fetch_assoc($result);
-
-  mysqli_free_result($result);
-  mysqli_close($conn);
-
 ?>
 
 <!DOCTYPE html>
@@ -35,6 +51,12 @@
         <p>Created at: <?php echo date($pizza['created_at']); ?></p>
         <h5>Ingredients:</h5>
         <p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+
+<!-- Delete Form -->
+<form action="details.php" method="POST">
+  <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id'] ?>" />
+  <input type="submit" value="Delete" name="delete" class="btn brand z-depth-0">
+</form>
 
       <?php else: ?>
         <h5>No such pizza exists!</h5>
